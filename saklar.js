6319d9c645592006c174fb53ae06069280ffa971
Naufall0000 <swastikanaufal@gmail.com>
2025-04-09 11:18:22 +0800

    memodifikasi saklar.js supaya lampu bisa menyala masing masing dengan tombolnya

--- saklar.js
+++ saklar.js
@@ -1,13 +1,31 @@
 //function lampu1
-function saklar(params) {
-    console.log(params);
+function saklar(action, lamp) {
     let lampu1 = document.getElementById("lampu1");
+    let lampu2 = document.getElementById("lampu2");
+    let lampu3 = document.getElementById("lampu3");
 
-    if(params == "on"){
-        lampu1.src = "assets/images/on.gif";
+    if(action == "on"){
+        if (lamp == 1) {
+            lampu1.src = "assets/images/on.gif";
+        }
+        if (lamp == 2) {
+            lampu2.src = "assets/images/on.gif";
+        }
+        if (lamp == 3) {
+            lampu3.src = "assets/images/on.gif";
+        }
+       
     }
-    if(params == "off"){
-        lampu1.src = "assets/images/off.gif";
+    if(action == "off"){
+        if (lamp == 1) {
+            lampu1.src = "assets/images/off.gif";
+        }
+        if (lamp == 2) {
+            lampu2.src = "assets/images/off.gif";
+        }
+        if (lamp == 3) {
+            lampu3.src = "assets/images/off.gif";
+        }
     }
 
     return lampu1;
